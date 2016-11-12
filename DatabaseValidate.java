ea99b2441ff3fe1f79d6342d80bf0150659bc2a7
Justin Linville <jlinville01@gmail.com>
2016-11-11 18:00:34 -0800

    Update DatabaseValidate.java

--- DatabaseValidate.java
+++ DatabaseValidate.java
@@ -1,28 +1,37 @@
-public void validatePerformancePulse(String id) throws Exception
+/**
+ * As a Manager account complete a four-question quiz.
+ *
+ * @param id	the managers id
+ */
+public void validateResponse(String id) throws Exception
 {
 	stmt = con.createStatement();
-	String runReset = "SELECT * FROM leader_evaluation WHERE manager_token_id = " + id + " ORDER BY uid DESC LIMIT 1";
-	ResultSet result = stmt.executeQuery(runReset);
+	
+	// Get the token for a Manager account using their ID
+	String searchForManager = "SELECT * FROM leader_evaluation WHERE manager_token_id = " + id + " ORDER BY uid DESC LIMIT 1";
+	ResultSet result = stmt.executeQuery(searchForManager);
 	this.con.commit();
 	while (result.next())
 	{
 		id = result.getString("uid");
 	}
-	Thread.sleep(1000);
+	
+	// Get the responses results for a Manager who completed a four-question test
 	runReset = "SELECT eq.sort_order, ero.option_text FROM leader_evaluation le JOIN evaluation_response er ON er.leader_evaluation_id = le.uid JOIN evaluation_response_option ero ON er.response_option_id = ero.uid JOIN evaluation_question eq ON ero.question_id = eq.uid WHERE le.uid = " 
 			+ id + " ORDER BY eq.sort_order ASC;";
 	result = stmt.executeQuery(runReset);
 	this.con.commit();
+	
+	// Compare the values from likert array to values saved in database
 	Integer i = 0;
-
 	while (result.next())
 	{
 		// Fetch values from DB to compare
-		String[] likert = {"Strongly Agree", "Strongly Agree", "Yes", "Yes"};
+		String[] likert = {"1", "2", "3", "4"};
 		String option_text = result.getString("option_text");
 		if (!(option_text.equals(likert[i])))
 		{
-			System.out.println("Error validating Performance Pulse");
+			System.out.println("Error validating responses");
 		}
 		i++;
 	}
